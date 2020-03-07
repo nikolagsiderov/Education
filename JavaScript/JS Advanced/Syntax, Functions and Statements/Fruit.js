9a031a48226300534aa332f0bd04e5d550f1f564
Nikola Siderov <iamnikkysiderov@gmail.com>
2020-03-08 00:06:05 +0300

    cancelled js advanced course

--- JavaScript/JS Advanced/Syntax, Functions and Statements/Fruit.js
+++
@@ -1,8 +0,0 @@
-function printResult(fruit, grams, pricePerKilo) {
-    let neededMoney = ((grams * pricePerKilo) / 1000).toFixed(2);
-    let kgAmount = (grams / 1000).toFixed(2);
-    
-    console.log(`I need $${neededMoney} to buy ${kgAmount} kilograms ${fruit}.`);
-}
-
-printResult('orange', 1563, 2.35);
