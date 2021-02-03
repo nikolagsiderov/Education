44528a5455044b3d8e9a2582abe2867edb0a4d4f
Nikola Siderov <iamnikkysiderov@gmail.com>
2021-02-03 16:00:13 +0200

    started algorithms course, it will be a tough one

---
+++ Algorithms/Recursion/NestedLoopsToRecursion/NestedLoopsToRecursion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NestedLoopsToRecursion
+{
+    class NestedLoopsToRecursion
+    {
+        static void Main(string[] args)
+        {
+            var input = int.Parse(Console.ReadLine());
+            //PrintNestedLoops(input);
+        }
+
+        // This solution is not mine
+        //private static void PrintNestedLoops(int loopCount)
+        //{
+        //    IterateNestedLoops(new Stack<int>(), 0, loopCount);
+        //}
+
+        //private static void IterateNestedLoops(Stack<int> nestedLoops, int currentIteration, int iterationCount)
+        //{
+        //    if (currentIteration == iterationCount)
+        //    {
+        //        Console.WriteLine(string.Join(" ", nestedLoops.Reverse()));
+        //        return;
+        //    }
+
+        //    for (int currentNumber = 1; currentNumber <= iterationCount; currentNumber++)
+        //    {
+        //        nestedLoops.Push(currentNumber);
+        //        IterateNestedLoops(nestedLoops, currentIteration + 1, iterationCount);
+        //        nestedLoops.Pop();
+        //    }
+        //}
+    }
+}
---
+++ Algorithms/Recursion/ReverseArray/ReverseArray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ReverseArray
+{
+    class ReverseArray
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var result = GetReversedArray(input);
+
+            Print(result);
+        }
+
+        private static string[] GetReversedArray(string[] input)
+        {
+            var reversedArr = new string[input.Length];
+            reversedArr = FillInReversedArr(reversedArr, 0, input);
+
+            return reversedArr;
+        }
+
+        private static string[] FillInReversedArr(string[] arr, int index, string[] input)
+        {
+            if (index == input.Length)
+                return arr;
+
+            arr[index] = input[input.Length - (index + 1)];
+            arr = FillInReversedArr(arr, index + 1, input);
+
+            return arr;
+        }
+
+        private static void Print(string[] arr)
+        {
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
