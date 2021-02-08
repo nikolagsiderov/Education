69183e5574cdfac6f4ad4617127cd40b726fae19
Nikola Siderov <iamnikkysiderov@gmail.com>
2021-02-08 11:22:26 +0200

    completed exercise for auto mapping objects

---
+++ Entity Framework Core/Automapping Objects/FastFood/FastFood.Web/Controllers/EmployeesController.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using FastFood.Data;
+using FastFood.Models;
+using FastFood.Web.ViewModels.Employees;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace FastFood.Web.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly FastFoodContext context;
+        private readonly IMapper mapper;
+
+        public EmployeesController(FastFoodContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public IActionResult Register()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterEmployeeInputModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            var employee = this.mapper.Map<Employee>(model);
+
+            this.context.Employees.Add(employee);
+            this.context.SaveChanges();
+
+            return this.RedirectToAction("All", "Employees");
+        }
+
+        public IActionResult All()
+        {
+            var employees = this.context.Employees
+                .ProjectTo<EmployeesAllViewModel>(mapper.ConfigurationProvider)
+                .ToList();
+
+            return this.View(employees);
+        }
+    }
+}
---
+++ Entity Framework Core/Automapping Objects/FastFood/FastFood.Web/MappingConfiguration/FastFoodProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using FastFood.Models;
+using FastFood.Web.ViewModels.Categories;
+using FastFood.Web.ViewModels.Employees;
+using FastFood.Web.ViewModels.Items;
+using FastFood.Web.ViewModels.Orders;
+using FastFood.Web.ViewModels.Positions;
+
+namespace FastFood.Web.MappingConfiguration
+{
+    public class FastFoodProfile : Profile
+    {
+        public FastFoodProfile()
+        {
+            this.CreateMap<CreatePositionInputModel, Position>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.PositionName)).ReverseMap();
+
+            this.CreateMap<CreateCategoryInputModel, Category>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.CategoryName)).ReverseMap();
+
+            this.CreateMap<RegisterEmployeeInputModel, Employee>().ReverseMap();
+
+            this.CreateMap<CreateItemInputModel, Item>().ReverseMap();
+
+            this.CreateMap<CreateOrderInputModel, Order>().ReverseMap();
+        }
+    }
+}
