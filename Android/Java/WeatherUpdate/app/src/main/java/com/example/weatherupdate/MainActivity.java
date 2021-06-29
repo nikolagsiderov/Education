675aa7a27392bc155a1d36b80853d5ee4a951f69
nikolagsiderov <iamnikkysiderov@gmail.com>
2021-06-29 03:09:18 +0300

    Added minor feature to the new Android app

--- Android/Java/WeatherUpdate/app/src/main/java/com/example/weatherupdate/MainActivity.java
+++ Android/Java/WeatherUpdate/app/src/main/java/com/example/weatherupdate/MainActivity.java
@@ -48,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
         String tempUrl = "";
         String city = etCity.getText().toString().trim();
         String country = etCountry.getText().toString().trim();
+
         if (city.equals("")) {
             tvResult.setText("City field can not be empty!");
         } else {
@@ -104,5 +105,57 @@ public class MainActivity extends AppCompatActivity {
             RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
             requestQueue.add(stringRequest);
         }
+    }
+
+    public void getWeatherDetailsForPlovdiv(View view) {
+        String city = "Plovdiv";
+        String tempUrl = url + "?q=" + city + "&appid=" + appid;
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, tempUrl, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    //Log.d("response", response);
+                    String output = "";
+                    try {
+                        JSONObject jsonResponse = new JSONObject(response);
+                        JSONArray jsonArray = jsonResponse.getJSONArray("weather");
+                        JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
+                        String description = jsonObjectWeather.getString("description");
+                        JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
+                        double temp = jsonObjectMain.getDouble("temp") - 273.15;
+                        double feelsLike = jsonObjectMain.getDouble("feels_like") - 273.15;
+                        float pressure = jsonObjectMain.getInt("pressure");
+                        int humidity = jsonObjectMain.getInt("humidity");
+                        JSONObject jsonObjectWind = jsonResponse.getJSONObject("wind");
+                        String wind = jsonObjectWind.getString("speed");
+                        JSONObject jsonObjectClouds = jsonResponse.getJSONObject("clouds");
+                        String clouds = jsonObjectClouds.getString("all");
+                        JSONObject jsonObjectSys = jsonResponse.getJSONObject("sys");
+                        String countryName = jsonObjectSys.getString("country");
+                        String cityName = jsonResponse.getString("name");
+
+                        output += "Прогнозата " +
+                                "за времето в " + cityName + " (" + countryName + ")"
+                                + "\n Температура: " + df.format(temp) + " Целзий"
+                                + "\n Усеща се като: " + df.format(feelsLike) + " Целзий"
+                                + "\n Влажност: " + humidity + "%"
+                                + "\n Детайли: " + description
+                                + "\n Скорост на вятъра: " + wind + "m/s (метра в секунда)"
+                                + "\n Облачност: " + clouds + "%"
+                                + "\n Налягане: " + pressure + " hPa";
+                        tvResult.setText(output);
+                    }
+                    catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(), error.toString().trim(), Toast.LENGTH_SHORT).show();
+                }
+            });
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(stringRequest);
     }
 }
